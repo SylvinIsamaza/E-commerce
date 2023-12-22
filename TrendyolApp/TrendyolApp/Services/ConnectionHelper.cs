c018ebfd5de3a164c9153b7480fb1d30d0e7db53
SylvinIsamaza <isamazasylvin@gmail.com>
2023-12-22 15:53:36 +0200

    style:Finishing database Setup

---
+++ TrendyolApp/TrendyolApp/Services/ConnectionHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrendyolApp.Services
+{
+    static class ConnectionHelper
+    {
+        public static string url = "https://192.168.1.3:45455/graphql/";
+    }
+}
